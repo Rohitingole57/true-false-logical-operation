61ee86483c3504f8a05d138eb142d1c42503b202
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:37:28 +0530

    javascript logical operation

---
+++ index.js
@@ -0,0 +1,14 @@
+var palak = true ;
+ var paneer =  true;
+
+
+ if(palak) {
+     if(paneer){
+         console.log("give me palak & paneer ");
+     }
+
+ }
+
+ if(palak && paneer){
+     console.log("give me palak & paneer ");
+ }
